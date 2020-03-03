236bc54db59b07fb68a0356adb7b4bb962da156c
Dmitriy <stadnit@mail.ru>
2020-03-03 20:36:34 +0300

    Ficha 4

--- Reader.cs
+++ Reader.cs
@@ -57,6 +57,13 @@ namespace _1_lab
         {
             System.IO.File.WriteAllText(path,data);
         }
+                /// <summary>
+        ///Append file by string
+        /// </summary>
+        public void AppendFile()
+        {
+            System.IO.File.AppendAllText(path,data);
+        }
 
     }
 }
